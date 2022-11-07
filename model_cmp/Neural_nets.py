a432a80a3dd9d2170fb8bac198662f61010e1a15
ayera-4 <100125289+ayera-4@users.noreply.github.com>
2022-11-07 18:27:47 +0300

    Update Neural_nets.py

--- model_cmp/Neural_nets.py
+++ model_cmp/Neural_nets.py
@@ -2,11 +2,11 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_boston, load_iris
 from sklearn.model_selection import train_test_split
 
-X = load_boston().data
-y = load_boston().target
+X = load_iris().data
+y = load_iris().target
 
 #create train, test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
@@ -39,10 +39,10 @@ input_shape = X_train[0].shape
 num_classes = y_train.shape[-1]
 
 model = model_def(input_shape, num_classes)
-model.compile(optimizer="ftrl", loss="mse", metrics=["mse"])
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
  
 history = model.fit(X_train, y_train, epochs=num_epochs)
 
 score = model.evaluate(X_test, y_test, verbose=0)
 print("Test loss:", score[0])
-print("Test MSE:", score[1])
+print("Test Accuracy:", score[1])
