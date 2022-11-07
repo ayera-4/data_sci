db6aa6e70001eecd69666c589db4a9497fd7b18b
ayera-4 <100125289+ayera-4@users.noreply.github.com>
2022-11-07 18:23:22 +0300

    Create Neural_nets.py

---
+++ model_cmp/Neural_nets.py
@@ -0,0 +1,48 @@
+#load dataset
+import pandas as pd
+import numpy as np
+
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+
+X = load_boston().data
+y = load_boston().target
+
+#create train, test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+#convert labels to one_hot encoding
+from sklearn.preprocessing import OneHotEncoder
+lb = OneHotEncoder()
+lb.fit(y_train)
+y_train = lb.transform(y_train)
+y_test = lb.transform(y_test)
+
+#Design neural network
+from tensorflow import keras
+def model_def(input_shape, num_classes):
+  #Build the architecture
+  model = keras.Sequential(
+      [
+          keras.layers.Dense(128, activation="relu", input_shape=input_shape),
+          keras.layers.Dense(64, activation="relu"),
+          keras.layers.Dense(32, activation="relu"),
+          keras.layers.Dense(num_classes, activation="softmax")
+      ]
+  )
+  print(model.summary())
+  return model
+
+#Train model
+num_epochs =50
+input_shape = X_train[0].shape
+num_classes = y_train.shape[-1]
+
+model = model_def(input_shape, num_classes)
+model.compile(optimizer="ftrl", loss="mse", metrics=["mse"])
+ 
+history = model.fit(X_train, y_train, epochs=num_epochs)
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print("Test loss:", score[0])
+print("Test MSE:", score[1])
